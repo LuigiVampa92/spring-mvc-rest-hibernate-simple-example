9e273ad8f1269adc3985e56b6e2e0915b5fc88f5
Pavel Vasiliev <luigivampa92@gmail.com>
2016-11-12 17:20:06 +0300

    added simple sl4j

--- src/main/java/com/luigivampa92/ingots/springmvc/rest/controller/UserController.java
+++ src/main/java/com/luigivampa92/ingots/springmvc/rest/controller/UserController.java
@@ -3,6 +3,8 @@ package com.luigivampa92.ingots.springmvc.rest.controller;
 import com.luigivampa92.ingots.springmvc.data.model.UserModel;
 import com.luigivampa92.ingots.springmvc.data.service.UserDataService;
 import com.luigivampa92.ingots.springmvc.rest.model.UserListResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -15,12 +17,16 @@ import java.util.List;
 @Controller
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserDataService userDataService;
 
     @RequestMapping(value = {"all", "all/"}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public UserListResponse getAllUsers() {
+        LOG.warn("getAllUsers() called");
+
         List<UserModel> allUsers = userDataService.getAllUsers();
         return new UserListResponse(allUsers);
     }
